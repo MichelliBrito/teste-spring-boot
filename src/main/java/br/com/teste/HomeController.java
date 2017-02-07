1da6fbdf69a2468527d60ffb43c0b25b6fb0366b
Michelli <michellidibrito@gmail.com>
2017-02-07 15:36:14 -0200

    enviando email

--- src/main/java/br/com/teste/HomeController.java
+++ src/main/java/br/com/teste/HomeController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import br.com.enviadorEmail.EmailService;
 import br.com.teste.model.Convidado;
 import br.com.teste.repository.ConvidadoRepository;
 
@@ -16,6 +17,7 @@ public class HomeController {
 
 	@Autowired
 	private ConvidadoRepository repository;
+	
 	
 	@RequestMapping("/teste")
     public String index() {
@@ -32,6 +34,8 @@ public class HomeController {
 	@RequestMapping(value= "/salvar", method = RequestMethod.POST)
 	public String salvar(Convidado convidado){
 		repository.save(convidado);
+		EmailService email = new EmailService();
+		email.enviar(convidado.getNome(), convidado.getEmail());
 		return "redirect:/listaconvidados";
 	}
 	
